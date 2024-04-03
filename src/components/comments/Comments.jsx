975b8fd338bcbcd1475819135bc7e08bf95cdaa9
Amir willosse <awillosse@gmail.com>
2024-04-03 10:48:57 +0100

    first commit

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,70 @@
+import { useContext, useState } from "react";
+import "./comments.scss";
+import { AuthContext } from "../../context/authContext";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+import moment from "moment";
+
+const Comments = ({ postId }) => {
+  const [descr, setDescr] = useState("");
+  const { currentUser } = useContext(AuthContext);
+
+  const { isLoading, error, data } = useQuery(["comments"], () =>
+    makeRequest.get("/comments?postId=" + postId).then((res) => {
+      return res.data;
+    })
+  );
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (newComment) => {
+      return makeRequest.post("/comments", newComment);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["comments"]);
+      },
+    }
+  );
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({ descr, postId });
+    setDescr("");
+  };
+
+  return (
+    <div className="comments">
+      <div className="write">
+        <img src={"http://localhost:8800/images/" + currentUser.profilepic} alt="" />
+        <input
+          type="text"
+          placeholder="write a comment"
+          value={descr}
+          onChange={(e) => setDescr(e.target.value)}
+        />
+        <button disabled={descr === "" ? true : false} onClick={handleClick}>Send</button>
+      </div>
+      {error
+        ? "Something went wrong"
+        : isLoading
+        ? "loading"
+        : data.map((comment) => (
+            <div className="comment"  comment={comment} key={comment.id}>
+              <img src={"http://localhost:8800/images/" + comment.profilepic} alt="" />
+              <div className="info">
+                <span>{comment.username}</span>
+                <p>{comment.descr}</p>
+              </div>
+              <span className="date">
+                {moment(comment.createdAt).fromNow()}
+              </span>
+            </div>
+          ))}
+    </div>
+  );
+};
+
+export default Comments;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+import "./navbar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link , useNavigate} from "react-router-dom";
+import { useContext, useState } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { AuthContext } from "../../context/authContext";
+import LogoutIcon from '@mui/icons-material/Logout';
+import axios from "axios";
+
+
+const Navbar = () => {
+  const { toggle, darkMode } = useContext(DarkModeContext);
+  const { currentUser } = useContext(AuthContext);
+  const [err , setErr] = useState()
+  const navigate = useNavigate()
+  const handleLogout = () => {
+
+    axios.post("http://localhost:8800/api/auth/logout").then((res)=>navigate("/login")).catch((err)=>setErr(true))
+  }
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>TRIXBOX</span>
+        </Link>
+        <Link to="/" style={{ textDecoration: "none" , color : "white"}}>
+         <HomeOutlinedIcon />
+        </Link>
+        
+        {darkMode ? (
+          <WbSunnyOutlinedIcon onClick={toggle} />
+        ) : (
+          <DarkModeOutlinedIcon onClick={toggle} />
+        )}
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="right">
+      <div onClick={handleLogout}>
+         <LogoutIcon />
+      </div>
+        {err && <div> Something wrong !</div>}
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <Link to={"/profile/" + currentUser.id} style={{ textDecoration: "none" }}>
+        <div className="user">
+          <img
+            src={"http://localhost:8800/images/" + currentUser.profilepic}
+            alt=""
+          />
+          <span>{currentUser.username}</span>
+        </div>
+        </Link>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
